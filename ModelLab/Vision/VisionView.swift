62efeae33598b0f5a7997c7525808141c9d8cf44
Ivan Milinkovic <ivanmcode@gmail.com>
2026-06-29 20:39:04 +0200

    Update VisionView.swift

--- ModelLab/Vision/VisionView.swift
+++ ModelLab/Vision/VisionView.swift
@@ -16,8 +16,7 @@ struct VisionView: View {
     var body: some View {
         VStack {
             if let captureSession = viewModel.session {
-                CameraPreview(session: captureSession)
-                    .overlay(points)
+                camera(session: captureSession)
                     .ignoresSafeArea()
             } else {
                 ProgressView()
@@ -40,10 +39,22 @@ struct VisionView: View {
                 Picker("", selection: $viewModel.detectionType) {
                     Text("Face").tag(VisionViewModel.DetectionType.face)
                     Text("Pose").tag(VisionViewModel.DetectionType.pose)
+                    Text("Eyes").tag(VisionViewModel.DetectionType.eyes)
                 }
                 .pickerStyle(.segmented)
             }
         }
+    }
+    
+    private func camera(session: AVCaptureSession) -> some View {
+        CameraPreview(session: session)
+            .overlay {
+                switch viewModel.detectionType {
+                case .face: points
+                case .pose: points
+                case .eyes: linesHistory
+                }
+            }
     }
     
     @ViewBuilder private var points: some View {
@@ -59,6 +70,37 @@ struct VisionView: View {
                     ctx.draw(Text(point.name), in: r2)
                 }
             }
+        }
+    }
+    
+    @ViewBuilder private var pointsHistory: some View {
+        Canvas { ctx, size in
+            let dotSize = 4.0
+            let color = colors[1]
+            for (i, point) in viewModel.eyeHistory.enumerated() {
+                let opacity = 0.5 * (1 - Double(i) / Double(viewModel.eyeHistory.count))
+                let x = point.x * size.width
+                let y = (1 - point.y) * size.height
+                let r = CGRect(x: x-dotSize/2, y: y-dotSize/2, width: dotSize, height: dotSize)
+                ctx.fill(Path(ellipseIn: r), with: .color(color.opacity(opacity)))
+            }
+        }
+    }
+    
+    @ViewBuilder private var linesHistory: some View {
+        Canvas { ctx, size in
+            let color = colors[1]
+            var path = Path()
+            for (i, point) in viewModel.eyeHistory.enumerated() {
+                let x = point.x * size.width
+                let y = (1 - point.y) * size.height
+                if i == 0 {
+                    path.move(to: .init(x: x, y: y))
+                } else {
+                    path.addLine(to: .init(x: x, y: y))
+                }
+            }
+            ctx.stroke(path, with: .color(color), style: .init(lineWidth: 8))
         }
     }
     
@@ -80,8 +122,11 @@ import Vision
     var message: String?
     var detectionType: DetectionType = .face
     
+    var detectionGroups: [DetectionGroup] = []
+    var eyeHistory: [CGPoint] = .init(repeating: .zero, count: 16)
+    
     enum DetectionType: CaseIterable {
-        case face, pose
+        case face, pose, eyes
     }
     
     func setup() async {
@@ -106,26 +151,35 @@ import Vision
             return
         }
         
-        switch detectionType {
-        case .pose:
-            detectionRequest = VNDetectHumanBodyPoseRequest()
-        case .face:
-            detectionRequest = VNDetectFaceLandmarksRequest()
-        }
+        detectionRequest = makeRequest()
         defer { detectionRequest = nil}
-        
-        let imageHandler = VNImageRequestHandler(cmSampleBuffer: buffer.value, orientation: orientation)
         do {
+            let imageHandler = VNImageRequestHandler(cmSampleBuffer: buffer.value, orientation: orientation)
             try imageHandler.perform([detectionRequest!])
             guard let observations = detectionRequest?.results else { return }
-            
-            if let observations = observations as? [VNHumanBodyPoseObservation] {
-                try processBodyPose(observations)
-            } else if let observations = observations as? [VNFaceObservation] {
-                processFaceLandmarks(observations)
-            }
+            try handle(observations: observations)
         } catch {
             message = error.localizedDescription
+        }
+    }
+    
+    private func makeRequest() -> VNImageBasedRequest {
+        switch detectionType {
+        case .pose:
+            VNDetectHumanBodyPoseRequest()
+        case .face, .eyes:
+            VNDetectFaceLandmarksRequest()
+        }
+    }
+    
+    private func handle(observations: [VNObservation]) throws {
+        switch detectionType {
+        case .pose:
+            try processBodyPose(observations as! [VNHumanBodyPoseObservation])
+        case .face:
+            processFaceLandmarks(observations as! [VNFaceObservation])
+        case .eyes:
+            processEyesLandmarks(observations as! [VNFaceObservation])
         }
     }
     
@@ -140,8 +194,6 @@ import Vision
         let coords: CGPoint
     }
     
-    var detectionGroups: [DetectionGroup] = []
-    
     func processBodyPose(_ observations: [VNHumanBodyPoseObservation]) throws {
         detectionGroups = try observations
             .filter { $0.confidence > 0.4 }
@@ -152,18 +204,46 @@ import Vision
             }
     }
     
+    let dtBetweenSnapshots: TimeInterval = 0.04
+    var lastSnapshotDate: Date = Date()
+    let onePoint = false
+    
     func processFaceLandmarks(_ observations: [VNFaceObservation]) {
         detectionGroups = observations.compactMap { face in
-        guard let landmarks = face.landmarks,
-              let allPoints = landmarks.allPoints else { return nil }
-        let box = face.boundingBox
-        let points = allPoints.normalizedPoints.map { pt in
-            let imageX = box.minX + pt.x * box.width
-            let imageY = box.minY + pt.y * box.height
-            return DetectionPoint(name: "", coords: CGPoint(x: imageX, y: imageY))
+            guard let landmarks = face.landmarks,
+                  let allPoints = landmarks.allPoints else { return nil }
+            let box = face.boundingBox
+            let points = allPoints.normalizedPoints.map { pt in
+                let imageX = box.minX + pt.x * box.width
+                let imageY = box.minY + pt.y * box.height
+                return DetectionPoint(name: "", coords: CGPoint(x: imageX, y: imageY))
+            }
+            return DetectionGroup(points: points)
         }
-        return DetectionGroup(points: points)
     }
+    
+    func processEyesLandmarks(_ observations: [VNFaceObservation]) {
+        guard let face = observations.first,
+              let leftEye: VNFaceLandmarkRegion2D = face.landmarks?.leftEye else { return }
+        
+        let points = leftEye.normalizedPoints
+        guard !points.isEmpty else { return }
+        let bbox = face.boundingBox
+        let centroid = points.reduce(CGPoint.zero) { acc, point in
+            CGPoint(x: acc.x + point.x,
+                    y: acc.y + point.y)
+        }
+        let normalizedCentroid = CGPoint(
+            x: centroid.x / CGFloat(points.count),
+            y: centroid.y / CGFloat(points.count)
+        )
+        let leftEyeCentroid = CGPoint(
+            x: bbox.minX + normalizedCentroid.x * bbox.width,
+            y: bbox.minY + normalizedCentroid.y * bbox.height
+        )
+        
+        eyeHistory.move(fromOffsets: IndexSet(integersIn: 0..<eyeHistory.count-1), toOffset: 1)
+        eyeHistory[0] = leftEyeCentroid
     }
     
     func start() async {
